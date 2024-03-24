e017ef8c651954bb5739d234376cc7b59bfbacb8
Hyperfied <hyperfied@outlook.com>
2024-03-24 16:47:05 +0000

    Initialise classes

---
+++ CMP1903_A2_2324/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903_A2_2324
+{
+    internal class Game
+    {
+
+    }
+
+    internal class SevensOut : Game
+    {
+
+    }
+
+    internal class ThreeOrMore : Game
+    {
+
+    }
+}
